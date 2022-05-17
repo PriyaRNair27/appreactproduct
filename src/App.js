b11d1585205862d73c4e61bf0efb790047d60485
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-17 23:54:12 +0530

    news

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Productentry from './Components/Productentry';
 
 function App() {
   return (
     <div>
-
+  <Productentry/>
     </div>
   );
 }
---
+++ src/Components/Productentry.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Productentry = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="className">productname</label>
+                    <input placeholder="enter product name" type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="className">price</label>
+                    <input placeholder="enter price" type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label className="className">Description</label>
+                    <textarea placeholder="enter description" name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-success">submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Productentry
