cd9df47e3f8787e38decf69f4a4388b9ab117d05
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-18 00:18:26 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Productentry from './Components/Productentry';
 import Search from './Components/Search';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-  <Productentry/>
-  <Search/>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/"  exact element={<Productentry/>}/>
+    <Route path="/search" exact element={<Search/>}/>
+  </Routes>
+  </BrowserRouter>
     </div>
   );
 }
