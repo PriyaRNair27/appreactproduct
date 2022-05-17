eca12cd8f3e272cf233802c9fc8bba05f02ea9c8
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-17 23:58:05 +0530

    news

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Productentry from './Components/Productentry';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div>
   <Productentry/>
+  <Search/>
     </div>
   );
 }
